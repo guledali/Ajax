ec9832be8b49eab4b63850202ea85767b2d7e26d
guledali <guled.ali01@gmail.com>
2017-01-24 21:57:24 +0100

    The Office Status Project made with jQuery
    Commit to master

---
+++ The Office Status Project Revisited/js/widget.js
@@ -0,0 +1,17 @@
+// ready function is jQuery thing waits for html in file to load before running script
+$(document).ready(function()  {
+  var url="../data/employees.json";
+    $.getJSON(url, function (response) {
+          var statusHTML = '<ul class="bulleted">';
+          $.each(response, function (index, employee)  {
+              if (employee.inoffice === true) {
+                statusHTML +='<li class="in">';
+              } else {
+                statusHTML +='<li class="out">';
+              }
+              statusHTML += employee.name + '</li>';
+          });
+          statusHTML += '</ul>';
+          $('#employeeList').html(statusHTML);
+  }); //end getJSON
+}); //end ready
